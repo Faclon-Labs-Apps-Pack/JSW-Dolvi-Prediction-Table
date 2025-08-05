a6b24ecceb65021baf016e1b82b1606f13ab92cf
Henil <132571378+HenilJainIO@users.noreply.github.com>
2025-08-06 02:46:53 +0530

    Update next.config.mjs

--- JSW-Dolvi-Prediction-Table/next.config.mjs
+++ JSW-Dolvi-Prediction-Table/next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // <-- Add this line to enable static export
   basePath: '/predictionTable',
   eslint: {
     ignoreDuringBuilds: true,
@@ -8,7 +9,7 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
-    unoptimized: true,
+    unoptimized: true, // Required for static export when using <Image>
   },
 }
 
